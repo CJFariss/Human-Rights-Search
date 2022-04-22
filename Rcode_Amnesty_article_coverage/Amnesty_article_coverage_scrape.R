6ae16b7675d3e00391d09b96e338fad18be02553
Christopher Fariss <christopherfariss@Christophers-MacBook-Pro.local>
2022-04-22 13:50:50 -0400

    search engine datasets

--- Rcode_Amnesty_article_coverage/Amnesty_article_coverage_scrape.R
+++ Rcode_Amnesty_article_coverage/Amnesty_article_coverage_scrape.R
@@ -18,13 +18,13 @@
 
 ## Do this (set to TRUE) to load libraries using the version from when the scripts were originally run
 if(FALSE){
-  ## load an older version of the ngramr library
+  ## load an older version of the libraries
   remotes::install_github('CredibilityLab/groundhog')
   library(groundhog)
   pkgs <- c("rvest")
   groundhog.library(pkgs,'2022-04-19')
 } else{
-  ## or load the more recent version of the ngramr library
+  ## or load the more recent version of the libraries
   install.packages("rvest")
   library(rvest)
 }
