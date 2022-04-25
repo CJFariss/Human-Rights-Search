50e827b43c1d4176e69a3bbdafc06d38611efd4c
Christopher Fariss <christopherfariss@ChristophersMBP.attlocal.net>
2022-04-25 14:06:18 -0400

    Updated amnesty.org scrapper. Save each search page results as a txt file for additional processing.

--- Rcode_Amnesty_article_coverage/Amnesty_article_coverage_scrape.R
+++ Rcode_Amnesty_article_coverage/Amnesty_article_coverage_scrape.R
@@ -33,56 +33,33 @@ if(FALSE){
 current_date <- as.Date(Sys.time())
 current_date
 
-pts <- read.csv("Data_input/PTS-2020.csv")
-
-#pts_names <- unique(pts$Country)
-#YEAR <- 1961:2021
-
-value <- c()
-for(i in 1892:nrow(pts)){
-  
-  YEAR <- pts$Year[i]
-  COUNTRY <- pts$Country[i]
-  
-  test <- try(readLines(paste("https://www.amnesty.org/en/search/",COUNTRY,"/?year=",YEAR,"&language=en", sep="")))
-  temp <- gsub("\t", "", test[500])
-  temp2 <- unlist(strsplit(temp, " "))
-  value[i] <- as.numeric(temp2[1])
-}
-
-pts_2015 <- subset(pts, Year>=2015)
-dim(pts_2015)
-value_2015 <- c()
-for(i in 1:nrow(pts_2015)){
-  
-  YEAR <- pts_2015$Year[i]
-  COUNTRY <- pts_2015$Country[i]
-  
-  test <- try(readLines(paste("https://www.amnesty.org/en/search/",COUNTRY,"/?year=",YEAR,"&language=en", sep="")))
-  temp <- gsub("\t", "", test[500])
-  temp2 <- unlist(strsplit(temp, " "))
-  value_2015[i] <- as.numeric(temp2[1])
-}
-
-boxplot(sqrt(pts_2015$amnesty_report_count) ~ pts_2015$PTS_S)
-boxplot(sqrt(pts_2015$amnesty_report_count) ~ pts_2015$PTS_A)
-boxplot(sqrt(pts_2015$amnesty_report_count) ~ pts_2015$PTS_H)
-write.csv(pts_2015, "amnesty_report_count_2015.csv", row.names=F)
-
-
+## load HRPS data for matching
 data <- readRDS("Data_input/HumanRightsProtectionScores_v4.01.Rdata")
 
+## add new variable for report counts
 data$amnesty_report_count <- NA
 
+## add new variable for country name associated with Amnesty report count
 data$country_name_amnesty <- data$country_name
 data$country_name_amnesty[data$country_name=="Myanmar (Burma) "] <- "Myanmar"
 
 #data$amnesty_report_count[data$country_name=="Myanmar (Burma) " & data$YEAR==2019] <- NA
 
-#data <- try(read.csv("/Users/christopherfariss/Dropbox/GOOGLEBOOK/SearchforRights\ 2/Data/HumanRightsProtectionScores_v4.01_amnesty_report_count.csv"))
+COUNTRY <- "Ukraine"
+YEAR <- 1999
+test <- try(readLines(paste("https://www.amnesty.org/en/search/",COUNTRY,"/?year=",YEAR,"&language=en", sep="")))
 
+## inspect test object
+length(test)
+grep(paste("results for", sep=""), test)
+test[grep(paste("results for", sep=""), test)]
+try(writeLines(text=test, con=paste("Rcode_Amnesty_article_coverage/Amnesty_webpage_source_files/Amnesty_source_", COUNTRY,"_",YEAR,".txt",sep="")))
+
+
+## loop through COUNTRY-YEAR combinations for scrapping at https://www.amnesty.org
+## Warning: this code takes a while and generates quite a few errors and warings, hence the try() function
 count <- 0
-for(i in 1:nrow(data)){
+out <- lapply(1:nrow(data), function(i){
   
   YEAR <- data$YEAR[i]
   COUNTRY <- data$country_name_amnesty[i]
@@ -91,51 +68,27 @@ for(i in 1:nrow(data)){
   if(is.na(data$amnesty_report_count[i]) & data$YEAR[i]>=1961 & data$YEAR[i]>=1961){
     test <- try(readLines(paste("https://www.amnesty.org/en/search/",COUNTRY,"/?year=",YEAR,"&language=en", sep="")))
     if(length(test)>0){
-      temp <- gsub("\t", "", test[500])
-      temp2 <- unlist(strsplit(temp, " "))
-      data$amnesty_report_count[i] <- as.numeric(temp2[1])
+      try(writeLines(text=test, con=paste("Rcode_Amnesty_article_coverage/Amnesty_webpage_source_files/Amnesty_source_", COUNTRY,"_",YEAR,".txt",sep="")))
       count <- count + 1
     }
   }
-}
+})
 
 count
-summary(data)
-
-year_cor <- c()
-YEAR <- 1961:2019
-for(i in 1:length(YEAR)){
-  year_cor[i] <- cor(data$theta_mean[data$YEAR==YEAR[i]], data$amnesty_report_count[data$YEAR==YEAR[i]], use="pairwise")
-}
-
-dat <- data.frame(year=1961:2019, year_cor)
-dat
-plot(dat$year, dat$year_cor, type="l", ylim=c(-.7,0))
-points(dat$year, dat$year_cor,)
-
-write.csv(data, paste("Data/HumanRightsProtectionScores_v4.01_amnesty_report_count",".csv"), row.names=FALSE)
-
-ctabs <- xtabs(data$amnesty_report_count ~ data$YEAR + data$country_name) 
-ctabs
-ctabs[,'Uganda']
-ctabs[,'Guatemala']
-ctabs[,'Argentina']
-ctabs[,'United Kingdom']
+#summary(data)
 
 
 
 count <- 0
-year_amnesty_report_count <- c()
+#year_amnesty_report_count <- c()
 YEAR <- 1961:2019
-for(i in 1:length(YEAR)){
+out <- lapply(1:length(YEAR), function(i){
   
   test <- temp <- temp2 <- c()
     test <- try(readLines(paste("https://www.amnesty.org/en/search/?year=",YEAR[i],"&language=en", sep="")))
     if(length(test)>0){
-      temp <- gsub("\t", "", test[500])
-      temp2 <- unlist(strsplit(temp, " "))
-      year_amnesty_report_count[i] <- as.numeric(temp2[1])
+      try(writeLines(text=test, con=paste("Rcode_Amnesty_article_coverage/Amnesty_webpage_source_files_YEAR/Amnesty_source_", YEAR,".txt",sep="")))
       count <- count + 1
     }
   
-}
+})
