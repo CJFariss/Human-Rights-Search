a73aadbd9992f618b36586dae2be25abcd80bfb3
Christopher Fariss <christopherfariss@ChristophersMBP.attlocal.net>
2022-04-26 19:50:24 -0400

    added min-max example code.

---
+++ Rcode_min_max/min_max_example_Rcode.R
@@ -0,0 +1,35 @@
+## min_max_example_Rcode.R
+##########################################################################
+##
+## Authors: Geoff Dancy and Christopher J. Fariss
+##
+## Title: "The Search for Human Rights: A Global Analysis Using Google Data"
+##
+## Contact Information: 
+##  Geoff Dancy <gdancy@tulane.edu>
+##  Christopher J. Fariss <cjf0006@gmail.com>
+##  
+##  Copyright (c) 2022, under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License.
+## For more information see: http://creativecommons.org/licenses/by-nc-sa/3.0/us/
+##  All rights reserved. 
+##
+##########################################################################
+
+search_volume <- c(100,50,10)
+hr_search_volume <- c(10,1,1)
+
+
+par(mfrow=c(4,1), mar=c(3,3,3,1))
+barplot(search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Total Search Volume")
+
+barplot(hr_search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Volume", ylim=c(0,10))
+
+search_ratio <- hr_search_volume/search_volume
+
+barplot(search_ratio, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Ratio", ylim=c(0,1))
+
+
+search_rate <- 100 * (search_ratio - min(search_ratio)) / (max(search_ratio) - min(search_ratio))
+
+barplot(search_rate, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Rate (min-max normalization)")
+
