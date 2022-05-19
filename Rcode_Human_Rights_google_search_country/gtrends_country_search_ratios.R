aa6d46b9e2432a5e11017a254ec16a897ca37d2e
Christopher Fariss <cjf0006@gmail.com>
2022-05-19 15:18:03 -0400

    updated gtrends_country_search_ratios_func

--- Rcode_Human_Rights_google_search_country/gtrends_country_search_ratios.R
+++ Rcode_Human_Rights_google_search_country/gtrends_country_search_ratios.R
@@ -1,9 +1,14 @@
 ## gtrends_country_search_ratios.R
 
+## define a function to work with the google trends object, specifically the interest_by_country data.frame
+gtrends_country_search_ratios_func <- function(dat, location1, location2){
+  as.numeric(dat$interest_by_country$hits[dat$interest_by_country$location==location1])/as.numeric(dat$interest_by_country$hits[dat$interest_by_country$location==location2])
+}
+
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2012-01-01_2016-12-31_saved_2022-05-11.RDS")
 
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2013-01-01_2017-12-31_saved_2022-05-11.RDS")
