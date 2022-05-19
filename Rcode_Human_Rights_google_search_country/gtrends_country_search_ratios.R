95bfc5b1e9e3eba0c784b65e1206c16a835efd79
Christopher Fariss <cjf0006@gmail.com>
2022-05-19 16:47:44 -0400

    added function

--- Rcode_Human_Rights_google_search_country/gtrends_country_search_ratios.R
+++ Rcode_Human_Rights_google_search_country/gtrends_country_search_ratios.R
@@ -7,51 +7,42 @@ gtrends_country_search_ratios_func <- function(dat, location1, location2){
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2012-01-01_2016-12-31_saved_2022-05-11.RDS")
-
 gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2013-01-01_2017-12-31_saved_2022-05-11.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2014-01-01_2018-12-31_saved_2022-05-11.RDS")
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Guatemala"]/dat$interest_by_country$hits[dat$interest_by_country$location=="Argentina"]
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_human_rights_2015-01-01_2019-12-31_saved_2022-05-11.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 
 ##
 dat <- readRDS("Data_output_global_search_lists_lowsearch/gsearch_global_data_lists_human_rights_2012-01-01_2016-12-31_saved_2022-05-12.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ##
 dat <- readRDS("Data_output_global_search_lists_lowsearch/gsearch_global_data_lists_human_rights_2013-01-01_2017-12-31_saved_2022-05-12.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ##
 dat <- readRDS("Data_output_global_search_lists_lowsearch/gsearch_global_data_lists_human_rights_2014-01-01_2018-12-31_saved_2022-05-12.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 ## 
 dat <- readRDS("Data_output_global_search_lists_lowsearch/gsearch_global_data_lists_human_rights_2015-01-01_2019-12-31_saved_2022-05-12.RDS")
-
-dat$interest_by_country$hits[dat$interest_by_country$location=="Uganda"]/dat$interest_by_country$hits[dat$interest_by_country$location=="United States"]
+gtrends_country_search_ratios_func(dat=dat, location1="Uganda", location2="United States")
 
 
 
 ##
 dat <- readRDS("Data_output_global_search_lists/gsearch_global_data_lists_derechos_humanos_2015-01-01_2019-12-31_saved_2022-05-11.RDS")
+gtrends_country_search_ratios_func(dat=dat, location1="Guatemala", location2="Argentina")
 
-as.numeric(dat$interest_by_country$hits[dat$interest_by_country$location=="Guatemala"])/as.numeric(dat$interest_by_country$hits[dat$interest_by_country$location=="Argentina"])
 
