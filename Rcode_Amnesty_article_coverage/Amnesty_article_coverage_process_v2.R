47388e838f8d516e25fd800b23e7839f7fd929b7
Christopher Fariss <cjf0006@gmail.com>
2022-05-21 10:42:38 -0400

    updated html processing

--- Rcode_Amnesty_article_coverage/Amnesty_article_coverage_process_v2.R
+++ Rcode_Amnesty_article_coverage/Amnesty_article_coverage_process_v2.R
@@ -27,7 +27,62 @@ for(i in 1:length(test_list)){
 id
 new_list[[id]]
 
+## testing
+table(sapply(new_list, length))
+which(sapply(new_list, length)>9)
 
-gsub(pattern="<article class=\"post post--result\" aria-label=\"", "", new_list[[5001]])
-gsub(pattern="\t<a class=\"floating-anchor\" href=\"", "", new_list[[5001]])
-gsub(pattern="\" aria-hidden=\"true\"></a>", "", new_list[[5001]])
+temp <- new_list[[900]]
+temp
+
+post_result_id <- grep("post post--result", temp)
+post_result_id
+post_href_id <- grep("floating-anchor", temp)
+post_href_id
+post_location_id <- grep("post-location", temp)
+post_location_id
+post_topic_id <- grep("post-topic", temp)
+post_topic_id
+post_title_id <- grep("post-title", temp)
+post_title_id
+post_excerpt_id <- grep("post-excerpt", temp)
+post_excerpt_id
+post_publication_date_id <- grep("Post published date", temp) 
+post_publication_date_id
+post_updated_date_id <- grep("Post updated date", temp)
+post_updated_date_id
+end_articl_id <- grep("</article>", temp) 
+end_articl_id
+
+## regular expression patterns to remove
+text_patterns <- c("<article class=\"post post--result\" aria-label=\"", 
+                  "\">", 
+                  "\t<a class=\"floating-anchor\" href=\"", 
+                  "\" aria-hidden=\"true\"></a>", 
+                  "\" aria-hidden=\"true</a>", 
+                  "\t<a class=\"post-location\" href=\"", 
+                  "\t<a class=\"post-topic\" href=\"",
+                  "\t<h1 class=\"post-title\"><a href=\"",
+                  "\t<h1 class=\"post-title<a href=\"",
+                  "\t<div class=\"post-excerpt\">",
+                  "\t<div class=\"post-excerpt",
+                  "</div>",
+                  "\t<span class=\"post-byline\" aria-label=\"Post published date\">",
+                  "</span>",
+                  "\t<strong class=\"post-byline\" aria-label=\"Post updated date\">",
+                  "\t<span class=\"post-byline\" aria-label=\"Post published date",
+                  "</strong>",
+                  "</a>",
+                  "</h1>"
+                  )
+
+for(k in 1:length(text_patterns)){
+  temp <- gsub(pattern=paste(text_patterns[k]), "", temp)
+}
+temp
+
+## regular expression patterns to use as seperators
+text_sep_patterns <- c("\" tabindex=\"0")
+for(k in 1:length(text_sep_patterns)){
+  temp <- gsub(pattern=paste(text_sep_patterns[k]), ";", temp)
+}
+temp
