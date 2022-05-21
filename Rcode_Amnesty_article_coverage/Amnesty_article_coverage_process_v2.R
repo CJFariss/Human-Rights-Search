d5c3e6f4e824e58c3926af240ee26b0b2e815a29
Christopher Fariss <cjf0006@gmail.com>
2022-05-21 01:59:01 -0400

    new amnesty.org web scrapper and process code

---
+++ Rcode_Amnesty_article_coverage/Amnesty_article_coverage_process_v2.R
@@ -0,0 +1,26 @@
+test_list <- readRDS("Rcode_Amnesty_article_coverage/Amnesty_webpage_source_files_v2/Amnesty_source_list_0001_5600.RDS")
+
+temp <- readRDS("Rcode_Amnesty_article_coverage/Amnesty_webpage_source_files_v2/Amnesty_source_list_5601_6260.RDS")
+
+test_list[5601:6260] <- temp[5601:6260]
+rm(temp)
+
+
+table(sapply(test_list[1:6260], length))
+
+
+new_list <- list()
+id <- 0
+
+for(i in 1:length(test_list)){
+#for(i in 1:5){
+  starts <- grep("<article class=", test_list[[i]])
+  stops <- grep("</article>", test_list[[i]])
+
+ for(j in 1:length(starts)){
+   id <- id + 1
+   coordinates <- starts[j]:stops[j]
+   new_list[[id]] <- test_list[[i]][coordinates]
+  } 
+  
+}
