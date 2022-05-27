de84e64d8a3ccabd091f87f28ff0ec477f8dac4a
Christopher Fariss <cjf0006@gmail.com>
2022-05-27 00:24:00 -0400

    updated co-occurance plots

--- Rcode_gtrends_related_topics/gtrends_related_topics.R
+++ Rcode_gtrends_related_topics/gtrends_related_topics.R
@@ -17,10 +17,16 @@ for(j in 1:length(files_names)){
   
   temp <- temp[which(sapply(temp, length)==7)]
     
+  
+  
   pdf(paste("Rplot_Related_Topics/related_topics_", gsub(".RDS", "", files_names[j]), ".pdf", sep=""), height=9, width=12)
   
   for(i in 1:length(temp)){
    
+    ## country name with error handling
+    NAME <- try(countrycode(head(temp[[i]]$interest_over_time$geo)[1], origin="iso2c", destination="country.name"))
+    if(grepl("Error", NAME)) NAME <- NA
+    
     par(mfrow=c(2,2), mar=c(2.5,20,2.25,1))
     
     if(!is.null(temp[[i]]$related_topics)){
@@ -29,7 +35,10 @@ for(j in 1:length(files_names)){
       related_topics$subject <- as.numeric(related_topics$subject)
       related_topics
       
-      if(any(!is.na(related_topics$subject))) barplot(related_topics$subject[order(related_topics$subject, decreasing=FALSE)], names.arg=related_topics$value[order(related_topics$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate for Related Topics", xaxt="n"); axis(side=1, at=c(0,20,40,60,80,100))
+      if(any(!is.na(related_topics$subject))){
+        barplot(related_topics$subject[order(related_topics$subject, decreasing=FALSE)], names.arg=related_topics$value[order(related_topics$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main=paste(NAME, ":\nRate for Related Topics", sep=""), xaxt="n")
+        axis(side=1, at=c(0,20,40,60,80,100))
+      }
       
     }
     
@@ -39,7 +48,10 @@ for(j in 1:length(files_names)){
       related_queries$subject <- as.numeric(related_queries$subject)
       related_queries
       
-      if(any(!is.na(related_queries$subject))) barplot(related_queries$subject[order(related_queries$subject, decreasing=FALSE)], names.arg=related_queries$value[order(related_queries$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0,  main="Rate for Related Searches (Queries)", xaxt="n"); axis(side=1, at=c(0,20,40,60,80,100))
+      if(any(!is.na(related_queries$subject))){ 
+        barplot(related_queries$subject[order(related_queries$subject, decreasing=FALSE)], names.arg=related_queries$value[order(related_queries$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0,  main=paste(NAME, ":\nRate of Related Searches", sep=""), xaxt="n")
+        axis(side=1, at=c(0,20,40,60,80,100))
+      }
       
     }
     
@@ -47,7 +59,10 @@ for(j in 1:length(files_names)){
       interest_by_city <- subset(temp[[i]]$interest_by_city, !is.na(hits))
       interest_by_city
       
-      if(any(!is.na(interest_by_city$hits))) barplot(interest_by_city$hits[order(interest_by_city$hits, decreasing=FALSE)], names.arg=interest_by_city$location[order(interest_by_city$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby City", xaxt="n"); axis(side=1, at=c(0,20,40,60,80,100))
+      if(any(!is.na(interest_by_city$hits))){
+        barplot(interest_by_city$hits[order(interest_by_city$hits, decreasing=FALSE)], names.arg=interest_by_city$location[order(interest_by_city$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main=paste(NAME, ":\nRate of Searches by City", sep=""), xaxt="n")
+        axis(side=1, at=c(0,20,40,60,80,100))
+      }
     
     }
     
@@ -56,7 +71,10 @@ for(j in 1:length(files_names)){
       interest_by_region
       
       
-      if(any(!is.na(interest_by_region$hits))) barplot(interest_by_region$hits[order(interest_by_region$hits, decreasing=FALSE)], names.arg=interest_by_region$location[order(interest_by_region$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby Region", xaxt="n"); axis(side=1, at=c(0,20,40,60,80,100))
+      if(any(!is.na(interest_by_region$hits))){
+        barplot(interest_by_region$hits[order(interest_by_region$hits, decreasing=FALSE)], names.arg=interest_by_region$location[order(interest_by_region$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main=paste(NAME, ":\nRate ofSearches by Region", sep=""), xaxt="n")
+        axis(side=1, at=c(0,20,40,60,80,100))
+      }
     }  
     
   }
