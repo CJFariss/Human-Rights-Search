818ddefa565294cf65eee66a1717df8f8524e947
Christopher Fariss <cjf0006@gmail.com>
2022-05-26 23:02:29 -0400

    related topics and search Rplots

--- Rcode_gtrends_related_topics/gtrends_related_topics.R
+++ Rcode_gtrends_related_topics/gtrends_related_topics.R
@@ -1,34 +1,42 @@
-## Ggtrends_elated_topics.R
+## gtrends_elated_topics.R
 
 temp <- readRDS("Data_output_location_search_lists_lowsearch/gsearch_location_data_lists_human_rights_2015-01-01_2019-12-31_saved_2022-05-12.RDS")
 
   
-related_topics <- subset(temp[[3]]$related_topics, related_topics=="top")
-related_topics
-related_queries <- subset(temp[[3]]$related_queries, related_queries=="top")
-related_queries
+pdf("Rplot_Related_Topics/gtrends_related_searches_2015_2019.pdf", height=9, width=12)
 
-related_topics$subject <- as.numeric(related_topics$subject)
-related_queries$subject <- as.numeric(related_queries$subject)
-related_queries
+for(i in 1:length(temp)){
+  related_topics <- subset(temp[[i]]$related_topics, related_topics=="top")
+  related_topics
+  
+  related_queries <- subset(temp[[i]]$related_queries, related_queries=="top")
+  related_queries
+  
+  related_topics$subject <- as.numeric(related_topics$subject)
+  related_queries$subject <- as.numeric(related_queries$subject)
+  related_queries
+  
+  interest_by_city <- subset(temp[[i]]$interest_by_city, !is.na(hits))
+  interest_by_city
+  
+  interest_by_region <- subset(temp[[i]]$interest_by_region, !is.na(hits))
+  interest_by_region
+  
+  par(mfrow=c(2,2), mar=c(2.5,18,2.25,.5))
+  
+  barplot(related_topics$subject[order(related_topics$subject, decreasing=FALSE)], names.arg=related_topics$value[order(related_topics$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate for Related Topics", xaxt="n")
+  axis(side=1, at=c(0,20,40,60,80,100))
+  
+  barplot(related_queries$subject[order(related_queries$subject, decreasing=FALSE)], names.arg=related_queries$value[order(related_queries$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0,  main="Rate for Related Searches (Queries)", xaxt="n")
+  axis(side=1, at=c(0,20,40,60,80,100))
+  
+  barplot(interest_by_city$hits[order(interest_by_city$hits, decreasing=FALSE)], names.arg=interest_by_city$location[order(interest_by_city$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby City", xaxt="n")
+  axis(side=1, at=c(0,20,40,60,80,100))
+  
+  barplot(interest_by_region$hits[order(interest_by_region$hits, decreasing=FALSE)], names.arg=interest_by_region$location[order(interest_by_region$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby Region", xaxt="n")
+  axis(side=1, at=c(0,20,40,60,80,100))
 
+}
 
-#pdf("Rplot_Related_Topics/Uganda_related_searches_2015_2019.pdf", height=6, width=8)
-
-par(mfrow=c(1,1), mar=c(2.5,21.5,2.25,.5))
-barplot(related_topics$subject[order(related_topics$subject, decreasing=FALSE)], names.arg=related_topics$value[order(related_topics$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate for Related Topics")
-barplot(related_queries$subject[order(related_queries$subject, decreasing=FALSE)], names.arg=related_queries$value[order(related_queries$subject, decreasing=FALSE)], horiz=TRUE, las=2, space=0,  main="Rate for Related Searches (Queries)")
-
-
-interest_by_city <- temp$interest_by_city
-interest_by_city <- subset(interest_by_city, !is.na(hits))
-
-interest_by_region <- temp$interest_by_region
-interest_by_region <- subset(interest_by_region, !is.na(hits))
-
-barplot(interest_by_city$hits[order(interest_by_city$hits, decreasing=FALSE)], names.arg=interest_by_city$location[order(interest_by_city$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby City")
-
-barplot(interest_by_region$hits[order(interest_by_region$hits, decreasing=FALSE)], names.arg=interest_by_region$location[order(interest_by_region$hits, decreasing=FALSE)], horiz=TRUE, las=2, space=0, main="Rate of Searches\nby Region")
-
-#dev.off()
+dev.off()
 
