888ebd002e9641397ae1470446ebb3572bc4eff5
Christopher Fariss <cjf0006@gmail.com>
2022-05-26 22:25:25 -0400

    updated min_max examples code and Rplots

--- Rcode_min_max_examples/min_max_transformation_example.R
+++ Rcode_min_max_examples/min_max_transformation_example.R
@@ -22,18 +22,22 @@ make_min_max_func <- function(search_volume = c(100,50,10), hr_search_volume = c
   
   par(mfrow=c(4,1), mar=c(3,3,3,1))
   
-  barplot(search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Total Search Volume", las=2)
+  barplot(search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Total Search Volume", las=1, yaxt="n")
+  axis(side=2, at=c(0,.25*max(search_volume), .5*max(search_volume), .75*max(search_volume), max(search_volume)), las=2)
   
-  barplot(hr_search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Volume", ylim=c(0,10), las=2)
+  barplot(hr_search_volume, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Volume", ylim=c(0,max(hr_search_volume)), las=1, yaxt="n")
+  axis(side=2, at=c(0,.25*max(hr_search_volume), .5*max(hr_search_volume), .75*max(hr_search_volume), max(hr_search_volume)), las=2)
   
   search_ratio <- hr_search_volume/search_volume
   
-  barplot(search_ratio, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Ratio", ylim=c(0,1), las=2)
+  barplot(search_ratio, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Ratio", ylim=c(0,1), las=1, yaxt="n")
+  axis(side=2, at=c(.0,.20,.40,.60,.80,1.00), las=2)
   
   
   search_rate <- 100 * (search_ratio - min(search_ratio)) / (max(search_ratio) - min(search_ratio))
   
-  barplot(search_rate, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Rate (min-max normalization)", las=2)
+  barplot(search_rate, names.arg=c("Country A", "Country B", "Country C"), main="Human Rights Search Rate (min-max normalization)", las=1, yaxt="n")
+  axis(side=2, at=c(0,20,40,60,80,100), las=2)
 
 }
 
